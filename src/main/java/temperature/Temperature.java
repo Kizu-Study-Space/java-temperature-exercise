ff1b3513ea995404b26714c632d285c950311ef1
zorafuchs <13620981+zorafuchs@users.noreply.github.com>
2023-10-03 21:01:03 +0200

    Solve exercise

---
+++ src/main/java/temperature/Temperature.java
@@ -0,0 +1,57 @@
+package temperature;
+
+public class Temperature {
+    private double celsius;
+
+    public Temperature () {
+        this.celsius = (double) 20;
+    }
+
+    public Temperature (final double celsius) {
+        this.celsius = celsius;
+    }
+
+    public double getCelsius(){
+        return this.celsius;
+    }
+
+    public void setCelsius(final double celsius){
+        this.celsius = celsius;
+    }
+
+    public double getKelvin() {
+        return this.celsius + (double) 273.15;
+    }
+
+    public double getFahrenheit() {
+        return (celsius * (double) 1.8) + (double) 32;
+    }
+
+    public void warmUpByCelsius(double warmUpBy) {
+        this.celsius += warmUpBy;
+    }
+
+    public void coolDownByCelsius(double coolDownBy) {
+        this.celsius -= coolDownBy;
+    }
+
+    public void warmUpByKelvin(double warmUpBy) {
+        this.warmUpByCelsius(warmUpBy);
+    }
+
+    public void coolDownByKelvin(double coolDownBy) {
+        this.coolDownByCelsius(coolDownBy);
+    }
+
+    public void warmUpByFahrenheit(float warmUpBy){
+        this.warmUpByCelsius(this.celsiusStepsFromFahrenheitSteps(warmUpBy));
+    }
+
+    public void coolDownByFahrenheit(float coolDownBy){
+        this.coolDownByCelsius(celsiusStepsFromFahrenheitSteps(coolDownBy));
+    }
+
+    private double celsiusStepsFromFahrenheitSteps(double fahrenheit) {
+        return fahrenheit / (double) 1.8;
+    }
+}
---
+++ src/test/java/temperature/TemperatureTest.java
@@ -0,0 +1,76 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import temperature.Temperature;
+
+public class TemperatureTest {
+
+    @Test
+    public void getCelsius() {
+        Temperature temperature = new Temperature(5);
+        assertEquals(5, temperature.getCelsius());
+        temperature = new Temperature();
+        assertEquals(20, temperature.getCelsius());
+    }
+
+    @Test
+    public void setCelsius() {
+        Temperature temperature = new Temperature();
+        temperature.setCelsius((double) 3);
+        assertEquals(3, temperature.getCelsius());
+    }
+
+    @Test
+    public void getFahrenheit() {
+        Temperature temperature = new Temperature((double) -40);
+        assertEquals((double) -40, temperature.getFahrenheit());
+    }
+
+    @Test
+    public void getKelvin() {
+        Temperature temperature = new Temperature((double) 20);
+        assertEquals( 293.15, temperature.getKelvin());
+    }
+
+    @Test
+    public void warmUpByCelsius() {
+        Temperature temperature = new Temperature((double) 20);
+        temperature.warmUpByCelsius(5);
+        assertEquals( 25, temperature.getCelsius());
+    }
+
+    @Test
+    public void coolDownByCelsius() {
+        Temperature temperature = new Temperature((double) 20);
+        temperature.coolDownByCelsius(5);
+        assertEquals( 15, temperature.getCelsius());
+    }
+
+    @Test
+    public void warmUpByKelvin() {
+        Temperature temperature = new Temperature((double) 20);
+        temperature.warmUpByKelvin(5);
+        assertEquals( 298.15, temperature.getKelvin());
+    }
+
+    @Test
+    public void coolDownByKelvin() {
+        Temperature temperature = new Temperature((double) 20);
+        temperature.coolDownByKelvin(3);
+        assertEquals( 290.15, temperature.getKelvin());
+    }
+
+    @Test
+    public void warmUpByFahrenheit() {
+        Temperature temperature = new Temperature((double) -40);
+        temperature.warmUpByFahrenheit(40);
+        assertEquals( 0, temperature.getFahrenheit());
+    }
+
+    @Test
+    public void coolDownByFahrenheit() {
+        Temperature temperature = new Temperature((double) -40);
+        temperature.coolDownByFahrenheit(40);
+        assertEquals( -80, temperature.getFahrenheit());
+    }
+}
