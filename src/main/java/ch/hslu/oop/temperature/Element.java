785cc5128fd0b22e15bac31f559597869db02f11
Zora Fuchs <zora.fuchs@stud.hslu.ch>
2023-11-13 21:52:04 +0100

    Safe Temperature inside an Element as a Temperature object instead of a kelvin double value

--- src/main/java/ch/hslu/oop/temperature/Element.java
+++ src/main/java/ch/hslu/oop/temperature/Element.java
@@ -4,29 +4,30 @@ public enum Element implements Comparable<Element> {
   N(7, "N", "Stickstoff", 63.15, 77.36),
   HG(80, "Hg", "Quecksilber", 234.32, 630.2),
   PB(82, "Pb", "Blei", 600.61, 2022);
-  private final double kelvinMeltingPoint;
-  private final double kelvinBoilingPoint;
+
+  private final Temperature meltingPoint;
+  private final Temperature boilingPoint;
   private final int atomicNumber;
   private final String name;
   private final String shortName;
   
-  Element (final int atomicNumber, final String shortName, final String name, final double kelvinMeltingPoint, final double kelvinBoilingPoint) {
-    this.kelvinMeltingPoint = kelvinMeltingPoint;
-    this.kelvinBoilingPoint = kelvinBoilingPoint;
+  Element (final int atomicNumber, final String shortName, final String name, final double meltingPoint, final double boilingPoint) {
+    this.meltingPoint = new Temperature(meltingPoint);
+    this.boilingPoint = new Temperature(boilingPoint);
     this.atomicNumber = atomicNumber;
     this.name = name;
     this.shortName = shortName;
   }
 
   final String getStateOfMatterAt(final Temperature temperature) {
-    if (temperature.getKelvin() < kelvinMeltingPoint) return StateOfMatter.SOLID.toGermanString(this, temperature);
-    if (temperature.getKelvin() < kelvinBoilingPoint) return StateOfMatter.LIQUID.toGermanString(this, temperature);
+    if (temperature.compareTo(meltingPoint) < 0) return StateOfMatter.SOLID.toGermanString(this, temperature);
+    if (temperature.compareTo(boilingPoint)  < 0) return StateOfMatter.LIQUID.toGermanString(this, temperature);
     return StateOfMatter.GAS.toGermanString(this, temperature);
   }
 
   @Override
   public String toString() {
-    return this.shortName + ": melting at " + this.kelvinMeltingPoint + "K, boiling at " + this.kelvinBoilingPoint + "K.";
+    return this.shortName + ": melting at " + this.meltingPoint.getKelvin() + "K, boiling at " + this.boilingPoint.getKelvin() + "K.";
   }
 
   public final int getAtomicNumber() {return this.atomicNumber;}
