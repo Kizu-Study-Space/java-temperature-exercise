fe6311f65843ea45be39f4c5f1b66e53fc31b82a
zorafuchs <13620981+zorafuchs@users.noreply.github.com>
2023-12-05 17:44:45 +0100

    Print average in main method

--- src/main/java/ch/hslu/oop/temperature/TemperatureUI.java
+++ src/main/java/ch/hslu/oop/temperature/TemperatureUI.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class TemperatureUI {
     public static void main(String[] args) {
+        TemperatureHistory temperatureHistory = new TemperatureHistory();
         String input;
         Scanner scanner = new Scanner(System.in);
         while(true) {
@@ -13,10 +14,19 @@ public class TemperatureUI {
             try {
                 Temperature temperature = Temperature.createWithKelvin(Double.valueOf(input));
                 System.out.println(temperature.toString());
+                temperatureHistory.add(temperature);
             } catch (NumberFormatException exception) {
                 System.out.println("Diese Zahl ist ungültig...");
             }
         }
+        System.out.println("================================");
+        System.out.println("|| Temperatur Zusammenfassung ||");
+        System.out.println("================================");
+        System.out.printf("|| Anzahl:             %d\n", temperatureHistory.getCount());
+        System.out.printf("|| Durchschnitt:       %.2fK\n", temperatureHistory.averageTemperature().getKelvin());
+        System.out.printf("|| Minimaltemperatur:  %.2fK\n", temperatureHistory.lowestTemperature().getKelvin());
+        System.out.printf("|| Maximaltemperatur:  %.2fK\n", temperatureHistory.highestTemperature().getKelvin());
+        System.out.println("================================");
         System.out.println("Programm beendet.");
     }
 }
